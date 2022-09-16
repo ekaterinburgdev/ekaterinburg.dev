f66cdb79081d7096ce2b051d801e7d70285c722d
Alexander Chabin <nibach@ya.ru>
2022-09-16 06:45:01 +0500

    Generate custom Mondrian logo

--- view/logo/index.js
+++ view/logo/index.js
@@ -1,3 +1,39 @@
-const logo = document.querySelector('[data-logo]')
+const mondrian = document.querySelector('[data-id="mondrian"]');
+const mondrianButton = document.querySelector('[data-id="mondrian-button"]');
 
-// TODO Animation with Mondrian
+renderMondrian();
+
+mondrianButton.addEventListener("click", () => {
+  renderMondrian();
+});
+
+function renderMondrian() {
+  while (mondrian.firstChild) {
+    mondrian.firstChild.remove();
+  }
+
+  for (let cell of generateMondrianItems()) {
+    mondrian.appendChild(cell);
+  }
+}
+
+
+function generateMondrianItems() {
+  let colors = ["black", "green", "blue", "yellow", "black", "black", "blue"];
+  let cells = [];
+
+  let random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+  
+  for (let i = 0; i < 100; i++) {
+    let span = random(1, 5);
+    let row = random(1, 12);
+    let colorIndex = random(1, colors.length);
+    const cell = document.createElement("div");
+    cell.style.gridColumn = `span ${span}`;
+    cell.style.gridRow = `span ${row}`;
+    cell.classList.add("logo__mondrian-item");
+    cell.classList.add(`logo__mondrian-item_${colors[colorIndex]}`);
+    cells.push(cell);
+  }
+  return cells;
+}
