e2e9bf7c89e18b13c09b848701a50e1b9184e641
Anton Akhatov <anton@gcor.ru>
2022-11-22 22:44:07 +0000

    feature: add circles preview

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 import './view/header';
-import './view/logo';
+import './view/preview';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   build: {
-    assetsDir: '.'
-  }
-})
+    assetsDir: ".",
+  },
+});
