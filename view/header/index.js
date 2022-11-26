192639168e9aa1f117419593ca060911a0953b77
Anton Ahatov <anton@gcor.ru>
2022-11-26 17:25:51 +0300

    feature: add preview with circles (#1)
    * feature: add circles preview
    
    * enable scroll animaiton
    
    * fix scroll to position
    
    * remove sticky position

--- view/header/index.js
+++ view/header/index.js
@@ -3,7 +3,7 @@ const headerMore = document.querySelector('[data-id="header-more"]');
 headerMore.addEventListener("click", (e) => {
   e.preventDefault();
   const target = document.querySelector(headerMore.getAttribute("href"));
-  const yPos = (target.getBoundingClientRect().top + target.getBoundingClientRect().bottom) / 1.99
+  const yPos = target.getBoundingClientRect().top + window.scrollY;
   smoothScrollTo(yPos, 10000);
 });
 
@@ -25,4 +25,5 @@ const smoothScrollTo = (yPos, duration) => {
 };
 
 // Easing function from https://gist.github.com/gre/1650294
-const easeInOutCubic = t => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
+const easeInOutCubic = (t) =>
+  t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
---
+++ view/preview/helpers.js
@@ -0,0 +1,47 @@
+export const SVG_DOCTYPE = "http://www.w3.org/2000/svg";
+
+export function getScrollPercent(scrollHeight) {
+  const doc = document.documentElement;
+  const scrollTop = doc.scrollTop || document.body.scrollTop;
+
+  return (scrollTop / (scrollHeight - doc.clientHeight)) * 100;
+}
+
+export function getDimensions() {
+  const doc = document.documentElement;
+
+  return {
+    vw: Math.max(doc.clientWidth || 0, window.innerWidth || 0),
+    vh: Math.max(doc.clientHeight || 0, window.innerHeight || 0),
+  };
+}
+
+export function useScroll({ element, onStartScroll, onScroll, onEndScroll }) {
+  let isScroll = false;
+  let timer = null;
+
+  window.addEventListener(
+    "scroll",
+    () => {
+      let scrollPosition = getScrollPercent(element.offsetHeight);
+
+      if (isScroll) {
+        onScroll(scrollPosition);
+
+        if (timer !== null) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(function () {
+          isScroll = false;
+          onEndScroll(scrollPosition);
+        }, 150);
+      } else {
+        onStartScroll(scrollPosition);
+        isScroll = true;
+      }
+    },
+    { passive: true }
+  );
+}
+
+export const checkIsMobile = () => window.innerWidth < 768;
