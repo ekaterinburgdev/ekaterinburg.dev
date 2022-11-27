aa25f67c0eb1e8b10e343dd7e1d462afbf6ab0bb
Omelekhin <pashaomelekhin@gmail.com>
2022-11-27 01:48:54 +0100

    Set colors from out design system

--- view/preview/index.js
+++ view/preview/index.js
@@ -15,7 +15,8 @@ const SELECTORS = {
 const CIRCLES = 30;
 const MIN_SCALE = 1.5;
 const MAX_SCALE = 4;
-const COLORS = ["#09b4fc", "#ffd201", "#01b302"];
+const COLORS = ["#00b4ff", "#ffd400", "#00b400"];
+// const COLORS = ["#fff000", "#00f000", "#00d2ff"];
 const ANIMATION = [
   // "linear",
   "easeOutQuad",
