55f644f9b986f535739304c36736add0639fe513
Alexander Chabin <nibach@ya.ru>
2023-04-03 00:02:25 +0500

    Remove unused JetBrains mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import "../../public/fonts/index.css";
-import "../../public/font-jetbrains-mono/index.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
