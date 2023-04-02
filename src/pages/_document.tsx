8da6294e17b8be52d14de7612123bea9a31f3b29
Anton Akhatov <anton@gcor.ru>
2023-04-02 14:41:31 +0100

    feature/add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,10 +45,31 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
 
         <link rel="dns-prefetch" href="https://mc.yandex.ru/" />
+
+        <script></script>
       </Head>
       <body>
         <Main />
         <NextScript />
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<script>
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+              ym(90436467, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true
+              });
+            </script>
+            `,
+          }}
+        />
       </body>
     </Html>
   );
