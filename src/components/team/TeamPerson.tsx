47bcd3032dc4f9b377507cdd17e9acdbf6a4f549
Alexander Chabin <nibach@ya.ru>
2023-04-10 12:07:54 +0500

    Add lazy loading to `<TeamPerson>`

--- src/components/team/TeamPerson.tsx
+++ src/components/team/TeamPerson.tsx
@@ -9,7 +9,7 @@ function Person({ name, role, photo }: ITeamPerson) {
     <figure className={cx("team-person__inner")}>
       {photo.length > 0 && (
         <div className={cx("team-person__photo")}>
-          <img src={photo} width={300} height={300} alt="" />
+          <img src={photo} width={300} height={300} alt="" loading="lazy" />
         </div>
       )}
       <figcaption className={cx("team-person__caption")}>
